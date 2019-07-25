9067edad66eae5419e0eb101cf18b485f701c2e2
ter40706 <TER01551@N20161200026.rootbrasil.intranet>
2019-07-25 15:01:28 -0300

    log post

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.json())
   .get('/', (req, res) => {
       res.send('e ae!');
 
       console.log("log")
   })
+  .post('/oauth', (req, res) => {
+        console.log(req.body);    
+    
+        res.send('{}');
+   })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
