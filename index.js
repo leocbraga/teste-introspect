5e2557ab409275ad0c82430bc10e0e50f97220ae
ter40706 <TER01551@N20161200026.rootbrasil.intranet>
2019-07-25 14:33:10 -0300

    log

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.send('e ae!s'))
+  .get('/', (req, res) => {
+      res.send('e ae!');
+
+      console.log("log")
+  })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
